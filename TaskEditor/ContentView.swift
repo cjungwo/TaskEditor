9ab50cc1a933f17bee5e9ab5d4931ff97c88a65e
Cjungwo <jwoo516@naver.com>
2024-09-02 16:10:44 +1000

    Initial Commit

---
+++ TaskEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskEditor
+//
+//  Created by JungWoo Choi on 2/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskEditor/TaskEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskEditorApp.swift
+//  TaskEditor
+//
+//  Created by JungWoo Choi on 2/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
