c2ab2327b7eaaba6f56ca5d89475121651e60d83
Cjungwo <jwoo516@naver.com>
2024-09-04 14:09:32 +1000

    #3 - setting custom colors and make main tab bar

---
+++ TaskEditor/Component/Constant/VXColor.swift
@@ -0,0 +1,17 @@
+//
+//  VXColor.swift
+//  TaskEditor
+//
+//  Created by JungWoo Choi on 4/9/2024.
+//
+
+import SwiftUI
+
+enum VXColor {
+  static let vxBlack = Color(.vxBlack)
+  static let vxIvory = Color(.vxIvory)
+  static let vxWhite = Color(.vxWhite)
+  static let vxOrange = Color(.vxOrange)
+  static let vxSkyblue = Color(.vxSkyblue)
+  static let vxDarkblue = Color(.vxDarkblue)
+}
--- TaskEditor/Feature/Tab/MainTabView.swift
+++ TaskEditor/Feature/Tab/MainTabView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct MainTabView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    TabView {
+      HomeView()
+        .tabItem {
+            Image(systemName: "house")
+        }
+
+      TodoListView()
+        .tabItem {
+            Image(systemName: "square.and.pencil")
+        }
+
+      MemoListView()
+        .tabItem {
+            Image(systemName: "note.text")
+        }
+
+      ProfileView()
+        .tabItem {
+            Image(systemName: "person")
+        }
     }
+    .tint(.vxDarkblue)
+  }
 }
 
 #Preview {
-    MainTabView()
+  MainTabView()
 }
