7e2691e3cc8e5033b08d9fa984a19ab071082d1c
Cjungwo <jwoo516@naver.com>
2024-09-06 18:49:20 +1000

    #7 - design HomeView

--- TaskEditor/Feature/Home/HomeView.swift
+++ TaskEditor/Feature/Home/HomeView.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
   var body: some View {
-      VStack {
-        HeaderView()
+    VStack {
+      HeaderView()
 
-        // Greeting
-        GreetingView()
+      GreetingView()
 
-        // today's todos
-        
-        Spacer()
-      }
+      TodayTaskListView()
+    }
   }
 }
 
@@ -32,19 +29,57 @@ private struct GreetingView: View {
           .frame(width: 36, height: 36)
           .bold()
 
-        Text("Hi, Chris")
+        Text("Hi, " + Mocks.mockUser.name)
           .font(.system(size: 42))
           .fontWeight(.heavy)
       }
 
-      Text("Today has " + "7" + "Tasks")
-        .font(.subheadline)
+      Spacer()
+        .frame(height: 16)
+
+      Text("Inspiring Idea")
+        .font(.system(size: 24))
+
+      Text(Mocks.mockIdeaList[1].title)
+        .frame(minHeight: 120)
     }
-    .frame(minHeight: 300)
-    .background(.green)
+  }
+}
+
+// MARK: - TodayTaskList
+private struct TodayTaskListView: View {
+  @State var todayTaskList: [Task] = Mocks.mockTaskList
+
+  fileprivate var body: some View {
+    VStack(alignment: .leading) {
+      Text("Today's \(todayTaskList.count )tasks")
+        .padding(.horizontal)
+
+      List(todayTaskList, id: \.id) { task in
+        HStack {
+          Button {
+            task.isDone.toggle()
+          } label: {
+            Image(systemName: task.isDone ? "checkmark.square.fill" :  "square")
+              .imageScale(.large)
+          }
+
+          VStack(alignment: .leading) {
+            Text(task.title)
+              .font(.title3)
+              .bold()
+
+            Text("\(task.dueDate.formatted(date: .abbreviated, time: .omitted))")
+              .font(.subheadline)
+              .foregroundStyle(.secondary)
+          }
+        }
+      }
+      .scrollIndicators(.hidden)
+    }
   }
 }
 
 #Preview {
-    HomeView()
+  HomeView()
 }
