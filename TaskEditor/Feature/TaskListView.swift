d36793af7bb482f8d5bea12d5635cc14df102d1c
Cjungwo <jwoo516@naver.com>
2024-09-07 12:40:22 +1000

    #10 - design TaskListView

--- TaskEditor/Feature/TaskListView.swift
+++ TaskEditor/Feature/TaskListView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct TaskListView: View {
+  @State var text: String = ""
     var body: some View {
-        Text("Task List View")
+      NavigationStack {
+        // TODO: - priority sorting
+        List {
+          ForEach(Mocks.mockTaskList, id: \.id) { task in
+            HStack {
+              Button {
+                //
+              } label: {
+                Image(systemName: task.isDone ? "checkmark.square.fill" : "square")
+                  .imageScale(.large)
+              }
+
+              VStack(alignment: .leading) {
+                Text(task.title)
+                  .bold()
+
+                if task.content != nil {
+                  Text(task.content!)
+                    .font(.subheadline)
+                    .lineLimit(1)
+                }
+              }
+
+              Spacer()
+
+              Text("\(task.dueDate.formatted(date: .abbreviated, time: .omitted))")
+            }
+          }
+          .onDelete(perform: { indexSet in
+            //
+          })
+        }
+        .searchable(text: $text)
+        .navigationTitle("Task List")
+        .toolbar {
+          ToolbarItem {
+            Button("Create") {
+              // TODO: - link CreateTaskView or CreateTaskSheet
+            }
+          }
+        }
+      }
     }
 }
 
