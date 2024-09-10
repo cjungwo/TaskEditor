a6cb74e9a5f6594e581f9326f6c8e8433fe1c7f5
Cjungwo <jwoo516@naver.com>
2024-09-10 20:02:35 +1000

    #18 - create base auth part

--- TaskEditor/Feature/Auth/SignIn/SignInView.swift
+++ TaskEditor/Feature/Auth/SignIn/SignInView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct SignInView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  @StateObject var viewModel = SignInViewModel()
+
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 #Preview {
-    SignInView()
+  SignInView()
 }
